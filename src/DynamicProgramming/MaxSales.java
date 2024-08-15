cfa3276deb5a89e193012695ecc1c0783e5aba64
String-Scorpion <wbc2001@126.com>
2024-08-15 20:27:18 +0800

    leetcode 53

--- src/DynamicProgramming/MaxSales.java
+++ src/DynamicProgramming/MaxSales.java
@@ -1,5 +1,6 @@
 package DynamicProgramming;
 
+/* Leetcode 53 */
 public class MaxSales {
     /**
      * 求最大的连续销售额
@@ -7,19 +8,25 @@ public class MaxSales {
      * @return 连续一或多天销售额总和的最大值
      */
     public static int maxSales(int[] sales) {
-        int earn = 0;
-        int max = sales[0];
+        /*
+         * 就是换皮“最大子数组”
+         */
+        int dp = 0; // 记录连续天数的最大和
+        int max = sales[0]; // 记录目前最大的和
         for (int sale : sales) {
-            int t = sale + earn;
-            earn = Math.max(t, sale);
-            max = Math.max(max, earn);
+            dp = Math.max(dp, 0) + sale;
+            max = Math.max(max, dp);
         }
         return max;
     }
 
     public static void main(String[] args) {
-        int[] prices = {-2,1,-3,4,-1,2,1,-5,4};
-        int sales = maxSales(prices);
-        System.out.println(sales);
+        int[] prices = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        /*
+         * 分析：
+         * {-2, 1, -3, [4, -1, 2, 1], -5, 4}
+         * max = 6
+         */
+        System.out.println(maxSales(prices));
     }
 }
