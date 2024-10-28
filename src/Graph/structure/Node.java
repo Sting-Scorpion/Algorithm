5cd0173ab772124934dac579e51c86018bdc14a0
String-Scorpion <wbc2001@126.com>
2024-10-28 13:30:00 +0800

    点的结构描述

--- src/Graph/structure/Node.java
+++ src/Graph/structure/Node.java
@@ -1,13 +1,18 @@
-package Graph;
+package Graph.structure;
 
 import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * 点的结构描述
+ */
 public class Node {
     public int value;
     public int in;  //入度
     public int out; //出度
-    public ArrayList<Node> nexts; //直接连通的点
-    public ArrayList<Edge> edges; //连接该点的边
+    public List<Node> nexts; //直接连通的点
+    public List<Edge> edges; //连接该点的边
+
     public Node(int val){
         value = val;
         in = 0;
