fe503ace6a6589570a37408c339e1c2362a7c4ef
String-Scorpion <wbc2001@126.com>
2024-10-28 13:29:52 +0800

    图的结构描述

--- src/Graph/structure/Graph.java
+++ src/Graph/structure/Graph.java
@@ -1,11 +1,17 @@
-package Graph;
+package Graph.structure;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+/**
+ * 图的结构描述
+ */
 public class Graph {
-    public HashMap<Integer, Node> nodes;
-    public HashSet<Edge> edges;
+    public Map<Integer, Node> nodes;
+    public Set<Edge> edges;
+
     public Graph(){
         nodes = new HashMap<>();
         edges = new HashSet<>();
