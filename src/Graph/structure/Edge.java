207b7fa5ab54fe397b9ea5cf1376201619021dcc
String-Scorpion <wbc2001@126.com>
2024-10-28 13:29:44 +0800

    边的结构描述

--- src/Graph/structure/Edge.java
+++ src/Graph/structure/Edge.java
@@ -1,9 +1,13 @@
-package Graph;
+package Graph.structure;
 
+/**
+ * 边的结构描述
+ */
 public class Edge {
     public int weight;
     public Node from;
     public Node to;
+
     public Edge(int w, Node f, Node t){
         weight = w;
         from = f;
